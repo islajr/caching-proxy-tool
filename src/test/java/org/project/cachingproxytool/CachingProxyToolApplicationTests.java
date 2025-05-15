3ef98e3e47746f9c05e7ab86d5ed90e99a480a35
Isla <islarcherjr@icloud.com>
2025-05-15 15:56:02 +0100

    initial commit.

---
+++ src/test/java/org/project/cachingproxytool/CachingProxyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.cachingproxytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingProxyToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
